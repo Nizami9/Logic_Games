ff8b76078f71150169bfaa3fcddfee15b96356e0
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-08-23 17:09:11 +0200

    changed code in NumGuessingGame

--- NumGuessingGame.py
+++ NumGuessingGame.py
@@ -1,38 +1,40 @@
 import random
 from art import logo2
 from replit import clear
-attempts = 0
 
 
 def play_more():
     new_game = input("Do you wanna play one more time? Type yes or no: ")
     if new_game == 'yes':
-        game()
+        game(attempts=3)
     else:
         return print("Have a nice day!")
 
 
-def check():
-    global attempts
+def check(attempts):
+    attempts = 0
     level = input("Choose a difficulty: easy or hard? ")
     if level == 'easy':
         attempts += 10
+        print(f"You have {attempts} attempts")
+        return attempts
     elif level == 'hard':
         attempts += 3
+        print(f"You have {attempts} attempts")
+        return attempts
     else:
         print('Wrong level. Please choose correct level')
-        check()
+        check(attempts)
 
 
-def game():
+def game(attempts):
     print(logo2)
-    global attempts
     print('Welcome to the Number Guessing Game!')
     print("I/'am thinking of a number between 1 and 100.")
 
     right_number = int(random.choice(range(10)))
 
-    check()
+    check(attempts)
 
     while attempts != 0:
         guess = int(input("Make a guess: "))
@@ -49,9 +51,10 @@ def game():
                 print(f"Guess again.")
                 print(f"You have {attempts} attempts remaining to guess the number")
 
+    print("Yo lose!")
     print(f"Right number was: {right_number}")
     clear()
     play_more()
 
 
-game()
+game(attempts=3)
