cc6feb1ac2c61f004bfefa72695e150d2122a675
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-09-05 17:07:38 +0200

    detected collision of snake with food and created scoreboard

--- Snake/SnakeGame.py
+++ Snake/SnakeGame.py
@@ -1,6 +1,9 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from Snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
 
 screen = Screen()
 
@@ -10,19 +13,22 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(snake.up, "Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
-
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
