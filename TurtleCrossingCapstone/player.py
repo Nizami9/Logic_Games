eae38bb6d951a122a57e9d65fb44e7a5d7d77152
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-09-07 13:32:30 +0200

    finished Turtle Crossing Capstone game

--- TurtleCrossingCapstone/player.py
+++ TurtleCrossingCapstone/player.py
@@ -10,7 +10,7 @@ class Player(Turtle):
         super().__init__()
         self.shape('turtle')
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
         # if self.player.ycor() >= FINISH_LINE_Y:
@@ -19,6 +19,15 @@ class Player(Turtle):
     def go_up(self):
         self.forward(MOVE_DISTANCE)
 
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
     # def go_left(self):
     #     new_y = self.ycor() - MOVE_DISTANCE
     #     self.player.goto(self.xcor(), new_y)
@@ -26,4 +35,3 @@ class Player(Turtle):
     # def go_right(self):
     #     new_y = self.ycor() + MOVE_DISTANCE
     #     self.player.goto(self.xcor(), new_y)
-
