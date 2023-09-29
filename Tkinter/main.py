39f0c589421a44e43a4d6d651f4dfdbf294e8da3
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-09-29 13:58:45 +0200

    added Pomodoro Project

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -6,13 +6,22 @@ window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text='I am a Label', font=("Arial", 24, "bold"))
-my_label.pack(side="left", expand=True)
+my_label.pack()
+
+my_label.config(text='New Label')
 
 
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 
+button = tkinter.Button(text='Click Me', command=button_clicked)
+button.pack()
 
 
-
+input = tkinter.Entry(width=10)
+input.pack()
+print(input.get())
 
 window.mainloop()
