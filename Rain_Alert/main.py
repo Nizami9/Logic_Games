e6013e3cd0d3ffb08d16b4e371a73dcc02706a24
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-10-12 17:20:55 +0200

    cretaed weather_app

---
+++ Rain_Alert/main.py
@@ -0,0 +1,42 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+owm_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = os.environ.get("OWN_api_key")
+account_sid = 'AC7b6778e2c1246b6ecf7f6f2c2119f882'
+auth_token = 'd1fd28d8603d285b1aa580aca0da776c'
+
+proxy_client = TwilioHttpClient()
+proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+weather_params = {
+    "lat": 52.150902,
+    "lon": 9.951000,
+    "appid": API_KEY,
+}
+
+response = requests.get(owm_endpoint, params=weather_params)
+weather_data = response.json()
+weather_slice = weather_data["list"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages.create(
+        body="Aı qız, başına papaq tax, xəstələnəssssssənç hava soyuq olacaq bugün!",
+        from_='+12369007006',
+        to='+4915209690036'
+    )
+
+    print(message.status)
+
+
+
