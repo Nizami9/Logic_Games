8810a12cefbf921051202497a3708113864facda
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-10-12 17:44:40 +0200

    finished rain_forecast

--- Rain_Alert/main.py
+++ Rain_Alert/main.py
@@ -4,12 +4,10 @@ from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 
 owm_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-API_KEY = os.environ.get("OWN_api_key")
+# API_KEY = os.environ.get("OWN_api_key")
+API_KEY = "4575bdd40aaca9e075c68d186cc58c2d"
 account_sid = 'AC7b6778e2c1246b6ecf7f6f2c2119f882'
 auth_token = 'd1fd28d8603d285b1aa580aca0da776c'
-
-proxy_client = TwilioHttpClient()
-proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
 weather_params = {
     "lat": 52.150902,
@@ -19,6 +17,7 @@ weather_params = {
 
 response = requests.get(owm_endpoint, params=weather_params)
 weather_data = response.json()
+print(weather_data)
 weather_slice = weather_data["list"][:12]
 
 will_rain = False
@@ -29,6 +28,9 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
     client = Client(account_sid, auth_token, http_client=proxy_client)
     message = client.messages.create(
         body="Aı qız, başına papaq tax, xəstələnəssssssənç hava soyuq olacaq bugün!",
