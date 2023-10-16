2ab66eb57301ae8ec647dd25a595d90de6eacb54
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-10-16 16:32:32 +0200

    made the first POST request to pixe.la

--- Habit_tracker/Main.py
+++ Habit_tracker/Main.py
@@ -1,14 +1,41 @@
 import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "nizami"
+TOKEN = 'dfjkdfjdfje34o43jf'
+GRAPH_ID = "graph1"
 
 user_params = {
-    "token": "dfjkdfjdfje34o43jf",
-    "username": "fakhri",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+
+pixel_config = {
+    "date": "20231010",
+    "quantity": '20',
+}
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+print(response.text)
 
